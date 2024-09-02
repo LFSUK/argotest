43a32619fd65f7cd77a05ca8537b4de6e496008b
skelton <skeltonlf@gmail.com>
2024-09-02 14:52:53 +0100

    Initial Load

---
+++ src/test/java/com/example/argotest/ArgotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.argotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgotestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
